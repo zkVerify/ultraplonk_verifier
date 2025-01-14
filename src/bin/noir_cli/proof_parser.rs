7bee709ae97814e3c120cd48fdd4076f63955857
DimitrisPapac <dimpapach87@gmail.com>
2025-01-14 22:54:17 +0100

    Debugged noir_cli utility to make compatible with Noir v.0.36.0

--- src/bin/noir_cli/proof_parser.rs
+++ src/bin/noir_cli/proof_parser.rs
@@ -1,183 +1,183 @@
-// // Copyright 2024, The Horizen Foundation
-// // SPDX-License-Identifier: Apache-2.0
-// //
-// // Licensed under the Apache License, Version 2.0 (the "License");
-// // you may not use this file except in compliance with the License.
-// // You may obtain a copy of the License at
-// //
-// //     http://www.apache.org/licenses/LICENSE-2.0
-// //
-// // Unless required by applicable law or agreed to in writing, software
-// // distributed under the License is distributed on an "AS IS" BASIS,
-// // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// // See the License for the specific language governing permissions and
-// // limitations under the License.
+// Copyright 2024, The Horizen Foundation
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-// use serde::Deserialize;
-// use std::fs::File;
-// use std::path::PathBuf;
+use serde::Deserialize;
+use std::fs::File;
+use std::path::PathBuf;
 
-// use crate::cli::Commands;
-// use crate::errors::CliError;
-// use crate::utils::{self, encode_hex, encode_pub_inputs, out_file};
+use crate::cli::Commands;
+use crate::errors::CliError;
+use crate::utils::{self, encode_hex, encode_pub_inputs, out_file};
 
-// #[derive(Deserialize, Debug)]
-// struct ProofData {
-//     proof: String,
-//     #[serde(rename = "verifyInputs")]
-//     verify_inputs: Vec<String>,
-// }
+#[derive(Deserialize, Debug)]
+struct ProofData {
+    proof: String,
+    #[serde(rename = "verifyInputs")]
+    verify_inputs: Vec<String>,
+}
 
-// pub fn process_proof_data(command: &Commands, verbose: bool) -> Result<(), CliError> {
-//     if let Commands::ProofData {
-//         input_json,
-//         output_proof,
-//         output_pubs,
-//     } = command
-//     {
-//         parse_proof_data(input_json, output_proof, output_pubs, verbose)
-//     } else {
-//         return Err(CliError::CliError("Invalid command".to_string()));
-//     }
-// }
+pub fn process_proof_data(command: &Commands, verbose: bool) -> Result<(), CliError> {
+    if let Commands::ProofData {
+        input_json,
+        output_proof,
+        output_pubs,
+    } = command
+    {
+        parse_proof_data(input_json, output_proof, output_pubs, verbose)
+    } else {
+        return Err(CliError::CliError("Invalid command".to_string()));
+    }
+}
 
-// fn parse_proof_data(
-//     input_json: &PathBuf,
-//     output_proof: &Option<PathBuf>,
-//     output_pubs: &Option<PathBuf>,
-//     verbose: bool,
-// ) -> Result<(), CliError> {
-//     if verbose {
-//         println!("Reading input JSON file: {:?}", input_json);
-//     }
+fn parse_proof_data(
+    input_json: &PathBuf,
+    output_proof: &Option<PathBuf>,
+    output_pubs: &Option<PathBuf>,
+    verbose: bool,
+) -> Result<(), CliError> {
+    if verbose {
+        println!("Reading input JSON file: {:?}", input_json);
+    }
 
-//     let json_path = input_json;
-//     let proof_data = read_json_file(json_path)?;
+    let json_path = input_json;
+    let proof_data = read_json_file(json_path)?;
 
-//     let mut proof_buf = vec![];
-//     let mut pub_inputs_buf = vec![];
+    let mut proof_buf = vec![];
+    let mut pub_inputs_buf = vec![];
 
-//     if verbose {
-//         println!("Encoding proof");
-//     }
+    if verbose {
+        println!("Encoding proof");
+    }
 
-//     encode_hex(&proof_data.proof, &mut proof_buf)?;
+    encode_hex(&proof_data.proof, &mut proof_buf)?;
 
-//     if verbose {
-//         println!("Encoding public inputs");
-//     }
+    if verbose {
+        println!("Encoding public inputs");
+    }
 
-//     encode_pub_inputs(&proof_data.verify_inputs, &mut pub_inputs_buf)?;
+    encode_pub_inputs(&proof_data.verify_inputs, &mut pub_inputs_buf)?;
 
-//     if verbose {
-//         println!("Writing output files");
-//     }
+    if verbose {
+        println!("Writing output files");
+    }
 
-//     out_file(output_proof.as_ref())?
-//         .write_all(&proof_buf)
-//         .map_err(|_| CliError::CliError("Failed to write output file".to_string()))?;
+    out_file(output_proof.as_ref())?
+        .write_all(&proof_buf)
+        .map_err(|_| CliError::CliError("Failed to write output file".to_string()))?;
 
-//     out_file(output_pubs.as_ref())?
-//         .write_all(&pub_inputs_buf)
-//         .map_err(|_| CliError::CliError("Failed to write output file".to_string()))?;
+    out_file(output_pubs.as_ref())?
+        .write_all(&pub_inputs_buf)
+        .map_err(|_| CliError::CliError("Failed to write output file".to_string()))?;
 
-//     return Ok(());
-// }
+    return Ok(());
+}
 
-// fn read_json_file(path: &std::path::PathBuf) -> Result<ProofData, CliError> {
-//     let file = File::open(path)
-//         .map_err(|_| CliError::CliError(format!("Failed to open JSON file: {:?}", path)))?;
-//     let reader = std::io::BufReader::new(file);
-//     let proof_data: ProofData = serde_json::from_reader(reader)
-//         .map_err(|_| CliError::CliError("Failed to parse JSON file".to_string()))?;
-//     Ok(proof_data)
-// }
+fn read_json_file(path: &std::path::PathBuf) -> Result<ProofData, CliError> {
+    let file = File::open(path)
+        .map_err(|_| CliError::CliError(format!("Failed to open JSON file: {:?}", path)))?;
+    let reader = std::io::BufReader::new(file);
+    let proof_data: ProofData = serde_json::from_reader(reader)
+        .map_err(|_| CliError::CliError("Failed to parse JSON file".to_string()))?;
+    Ok(proof_data)
+}
 
-// pub fn process_proof_data_v2(command: &Commands, verbose: bool) -> Result<(), CliError> {
-//     if let Commands::ProofDatav2 {
-//         num_inputs,
-//         input_proof,
-//         output_proof,
-//         output_pubs,
-//     } = command
-//     {
-//         parse_proof_data_v2(num_inputs, input_proof, output_proof, output_pubs, verbose)
-//     } else {
-//         return Err(CliError::CliError("Invalid command".to_string()));
-//     }
-// }
+pub fn process_proof_data_v2(command: &Commands, verbose: bool) -> Result<(), CliError> {
+    if let Commands::ProofDatav2 {
+        num_inputs,
+        input_proof,
+        output_proof,
+        output_pubs,
+    } = command
+    {
+        parse_proof_data_v2(num_inputs, input_proof, output_proof, output_pubs, verbose)
+    } else {
+        return Err(CliError::CliError("Invalid command".to_string()));
+    }
+}
 
-// fn parse_proof_data_v2(
-//     num_inputs: &usize,
-//     input_proof: &PathBuf,
-//     output_proof: &Option<PathBuf>,
-//     output_pubs: &Option<PathBuf>,
-//     verbose: bool,
-// ) -> Result<(), CliError> {
-//     if verbose {
-//         println!("Parsing proof");
-//     }
-//     // Parse proof and strip it from the pub ins
-//     let mut proof = std::fs::read(input_proof).map_err(|e| {
-//         CliError::CliError(format!(
-//             "Failed to read file: {:?}. Reason :{:?}",
-//             input_proof, e
-//         ))
-//     })?;
+fn parse_proof_data_v2(
+    num_inputs: &usize,
+    input_proof: &PathBuf,
+    output_proof: &Option<PathBuf>,
+    output_pubs: &Option<PathBuf>,
+    verbose: bool,
+) -> Result<(), CliError> {
+    if verbose {
+        println!("Parsing proof");
+    }
+    // Parse proof and strip it from the pub ins
+    let mut proof = std::fs::read(input_proof).map_err(|e| {
+        CliError::CliError(format!(
+            "Failed to read file: {:?}. Reason :{:?}",
+            input_proof, e
+        ))
+    })?;
 
-//     let expected_len = ultraplonk_no_std::PROOF_SIZE + 32 * num_inputs;
-//     if proof.len() != expected_len {
-//         return Err(CliError::CliError(format!(
-//             "File size is not as expected. Expected {:?}, Actual: {:?}",
-//             expected_len,
-//             proof.len()
-//         )));
-//     }
+    let expected_len = ultraplonk_no_std::PROOF_SIZE + 32 * num_inputs;
+    if proof.len() != expected_len {
+        return Err(CliError::CliError(format!(
+            "File size is not as expected. Expected {:?}, Actual: {:?}",
+            expected_len,
+            proof.len()
+        )));
+    }
 
-//     if verbose {
-//         println!("Parsing public inputs");
-//     }
+    if verbose {
+        println!("Parsing public inputs");
+    }
 
-//     let proof_without_pubs = proof.split_off(32 * num_inputs);
+    let proof_without_pubs = proof.split_off(32 * num_inputs);
 
-//     if verbose {
-//         println!("Writing output files");
-//     }
+    if verbose {
+        println!("Writing output files");
+    }
 
-//     // Write output proof in binary format
-//     out_file(output_proof.as_ref())?
-//         .write_all(&proof_without_pubs)
-//         .map_err(|_| CliError::CliError("Failed to write output file".to_string()))?;
+    // Write output proof in binary format
+    out_file(output_proof.as_ref())?
+        .write_all(&proof_without_pubs)
+        .map_err(|_| CliError::CliError("Failed to write output file".to_string()))?;
 
-//     // Write proof in hex format
-//     let mut w = out_file(
-//         output_proof
-//             .as_ref()
-//             .map(|out_path| out_path.with_extension("hex"))
-//             .as_ref(),
-//     )?;
-//     utils::dump_data_hex(&mut w, &proof_without_pubs)
-//         .map_err(|_| CliError::CliError("Failed to write output file".to_string()))?;
+    // Write proof in hex format
+    let mut w = out_file(
+        output_proof
+            .as_ref()
+            .map(|out_path| out_path.with_extension("hex"))
+            .as_ref(),
+    )?;
+    utils::dump_data_hex(&mut w, &proof_without_pubs)
+        .map_err(|_| CliError::CliError("Failed to write output file".to_string()))?;
 
-//     // Write output pub ins
-//     out_file(output_pubs.as_ref())?
-//         .write_all(&proof)
-//         .map_err(|_| CliError::CliError("Failed to write output file".to_string()))?;
+    // Write output pub ins
+    out_file(output_pubs.as_ref())?
+        .write_all(&proof)
+        .map_err(|_| CliError::CliError("Failed to write output file".to_string()))?;
 
-//     // Write pub ins in hex format
-//     let mut w = out_file(
-//         output_pubs
-//             .as_ref()
-//             .map(|out_path| out_path.with_extension("hex"))
-//             .as_ref(),
-//     )?;
+    // Write pub ins in hex format
+    let mut w = out_file(
+        output_pubs
+            .as_ref()
+            .map(|out_path| out_path.with_extension("hex"))
+            .as_ref(),
+    )?;
 
-//     let pubs_vec = crate::verifier::convert_to_pub_inputs(&proof)?;
+    let pubs_vec = crate::verifier::convert_to_pub_inputs(&proof)?;
 
-//     for ins in pubs_vec {
-//         utils::dump_data_hex(&mut w, ins)
-//             .map_err(|_| CliError::CliError("Failed to write output file".to_string()))?;
-//     }
+    for ins in pubs_vec {
+        utils::dump_data_hex(&mut w, ins)
+            .map_err(|_| CliError::CliError("Failed to write output file".to_string()))?;
+    }
 
-//     Ok(())
-// }
+    Ok(())
+}
--- src/bin/noir_cli/utils.rs
+++ src/bin/noir_cli/utils.rs
@@ -1,130 +1,131 @@
-// // Copyright 2024, The Horizen Foundation
-// // SPDX-License-Identifier: Apache-2.0
-// //
-// // Licensed under the Apache License, Version 2.0 (the "License");
-// // you may not use this file except in compliance with the License.
-// // You may obtain a copy of the License at
-// //
-// //     http://www.apache.org/licenses/LICENSE-2.0
-// //
-// // Unless required by applicable law or agreed to in writing, software
-// // distributed under the License is distributed on an "AS IS" BASIS,
-// // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// // See the License for the specific language governing permissions and
-// // limitations under the License.
+// Copyright 2024, The Horizen Foundation
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-// use crate::errors::CliError;
-// use regex::Regex;
-// use std::fs::File;
-// use std::io::Write;
+use crate::errors::CliError;
+use regex::Regex;
+use std::fs::File;
+use std::io::Write;
 
-// pub fn out_file(output: Option<&std::path::PathBuf>) -> Result<Box<dyn std::io::Write>, CliError> {
-//     // Attempt to create the file if a path is specified
-//     let from_path = output
-//         .map(|p| {
-//             // Try to create the file and add context to any error that occurs
-//             File::create(&p)
-//                 .map_err(|_| CliError::CliError(format!("Failed to create output file {:?}", &p)))
-//         })
-//         .transpose()? // Convert Option<Result<File>> to Result<Option<File>>
-//         .map(|f| Box::new(f) as Box<dyn Write>); // Box the file writer
+pub fn out_file(output: Option<&std::path::PathBuf>) -> Result<Box<dyn std::io::Write>, CliError> {
+    // Attempt to create the file if a path is specified
+    let from_path = output
+        .map(|p| {
+            // Try to create the file and add context to any error that occurs
+            File::create(&p)
+                .map_err(|_| CliError::CliError(format!("Failed to create output file {:?}", &p)))
+        })
+        .transpose()? // Convert Option<Result<File>> to Result<Option<File>>
+        .map(|f| Box::new(f) as Box<dyn Write>); // Box the file writer
 
-//     // If no path is specified, default to stdout
-//     Ok(from_path.unwrap_or_else(|| Box::new(std::io::stdout()) as Box<dyn Write>))
-// }
+    // If no path is specified, default to stdout
+    Ok(from_path.unwrap_or_else(|| Box::new(std::io::stdout()) as Box<dyn Write>))
+}
 
-// pub fn encode_value_as_u32(
-//     value: &str,
-//     variable_part: &str,
-//     buf: &mut [u8],
-//     offset: &mut usize,
-// ) -> Result<(), CliError> {
-//     encode_value(value, variable_part, buf, offset, 4)
-// }
+pub fn encode_value_as_u32(
+    value: &str,
+    variable_part: &str,
+    buf: &mut [u8],
+    offset: &mut usize,
+) -> Result<(), CliError> {
+    encode_value(value, variable_part, buf, offset, 4)
+}
 
-// pub fn encode_value_as_u256(
-//     value: &str,
-//     variable_part: &str,
-//     buf: &mut [u8],
-//     offset: &mut usize,
-// ) -> Result<(), CliError> {
-//     encode_value(value, variable_part, buf, offset, 32)
-// }
+pub fn encode_value_as_u256(
+    value: &str,
+    variable_part: &str,
+    buf: &mut [u8],
+    offset: &mut usize,
+) -> Result<(), CliError> {
+    encode_value(value, variable_part, buf, offset, 32)
+}
 
-// pub fn encode_str(key: &str, buf: &mut [u8], offset: &mut usize) -> Result<(), CliError> {
-//     encode_u32(key.len() as u32, buf, offset);
+pub fn encode_str(key: &str, buf: &mut [u8], offset: &mut usize) -> Result<(), CliError> {
+    encode_u32(key.len() as u32, buf, offset);
 
-//     buf[*offset..*offset + key.len()].copy_from_slice(key.as_bytes());
-//     *offset += key.len();
-//     Ok(())
-// }
+    buf[*offset..*offset + key.len()].copy_from_slice(key.as_bytes());
+    *offset += key.len();
+    Ok(())
+}
 
-// pub fn encode_u32(value: u32, buf: &mut [u8], offset: &mut usize) {
-//     buf[*offset..*offset + 4].copy_from_slice(&value.to_be_bytes());
-//     *offset += 4;
-// }
+pub fn encode_u32(value: u32, buf: &mut [u8], offset: &mut usize) {
+    buf[*offset..*offset + 4].copy_from_slice(&value.to_be_bytes());
+    *offset += 4;
+}
 
-// pub fn encode_value(
-//     value: &str,
-//     variable_part: &str,
-//     buf: &mut [u8],
-//     offset: &mut usize,
-//     length: usize,
-// ) -> Result<(), CliError> {
-//     let pattern = format!(
-//         r"mstore\(add\(_vk, {}\), (0x[0-9a-fA-F]{{64}})\)",
-//         regex::escape(variable_part)
-//     );
+#[cfg(feature = "alloc")]
+pub fn encode_value(
+    value: &str,
+    variable_part: &str,
+    buf: &mut [u8],
+    offset: &mut usize,
+    length: usize,
+) -> Result<(), CliError> {
+    let pattern = format!(
+        r"mstore\(add\(_vk, {}\), (0x[0-9a-fA-F]{{64}})\)",
+        regex::escape(variable_part)
+    );
 
-//     let re = Regex::new(&pattern).map_err(|_| {
-//         CliError::CliError(format!("Failed to compile regex pattern: {:?}", pattern))
-//     })?;
+    let re = Regex::new(&pattern).map_err(|_| {
+        CliError::CliError(format!("Failed to compile regex pattern: {:?}", pattern))
+    })?;
 
-//     if let Some(cap) = re.captures(value) {
-//         let hex_value = &cap[1];
-//         let decoded_value = hex::decode(hex_value.strip_prefix("0x").unwrap_or(hex_value))
-//             .map_err(|_| {
-//                 CliError::CliError(format!("Failed to decode hex string: {:?}", hex_value))
-//             })?;
-//         let decoded_value = &decoded_value[32 - length..];
+    if let Some(cap) = re.captures(value) {
+        let hex_value = &cap[1];
+        let decoded_value = hex::decode(hex_value.strip_prefix("0x").unwrap_or(hex_value))
+            .map_err(|_| {
+                CliError::CliError(format!("Failed to decode hex string: {:?}", hex_value))
+            })?;
+        let decoded_value = &decoded_value[32 - length..];
 
-//         if buf.len() < *offset + decoded_value.len() {
-//             return Err(CliError::CliError(format!(
-//                 "Buffer is too small: {} < {}",
-//                 buf.len(),
-//                 *offset + decoded_value.len()
-//             )));
-//         }
+        if buf.len() < *offset + decoded_value.len() {
+            return Err(CliError::CliError(format!(
+                "Buffer is too small: {} < {}",
+                buf.len(),
+                *offset + decoded_value.len()
+            )));
+        }
 
-//         buf[*offset..*offset + decoded_value.len()].copy_from_slice(&decoded_value);
-//         *offset += decoded_value.len();
-//         Ok(())
-//     } else {
-//         Err(CliError::CliError(format!(
-//             "Failed to extract value from: {:?}",
-//             variable_part
-//         )))
-//     }
-// }
+        buf[*offset..*offset + decoded_value.len()].copy_from_slice(&decoded_value);
+        *offset += decoded_value.len();
+        Ok(())
+    } else {
+        Err(CliError::CliError(format!(
+            "Failed to extract value from: {:?}",
+            variable_part
+        )))
+    }
+}
 
-// pub fn encode_hex(hex_str: &str, buf: &mut Vec<u8>) -> Result<(), CliError> {
-//     let decoded_value = hex::decode(hex_str.strip_prefix("0x").unwrap_or(hex_str))
-//         .map_err(|_| CliError::CliError(format!("Failed to decode hex string: {:?}", hex_str)))?;
-//     buf.extend_from_slice(&decoded_value);
-//     Ok(())
-// }
+pub fn encode_hex(hex_str: &str, buf: &mut Vec<u8>) -> Result<(), CliError> {
+    let decoded_value = hex::decode(hex_str.strip_prefix("0x").unwrap_or(hex_str))
+        .map_err(|_| CliError::CliError(format!("Failed to decode hex string: {:?}", hex_str)))?;
+    buf.extend_from_slice(&decoded_value);
+    Ok(())
+}
 
-// pub fn encode_pub_inputs(pub_inputs: &[String], buf: &mut Vec<u8>) -> Result<(), CliError> {
-//     for input in pub_inputs {
-//         let decoded_value = hex::decode(input.strip_prefix("0x").unwrap_or(input))
-//             .map_err(|_| CliError::CliError(format!("Failed to decode hex string: {:?}", input)))?;
-//         buf.extend_from_slice(&decoded_value);
-//     }
-//     Ok(())
-// }
+pub fn encode_pub_inputs(pub_inputs: &[String], buf: &mut Vec<u8>) -> Result<(), CliError> {
+    for input in pub_inputs {
+        let decoded_value = hex::decode(input.strip_prefix("0x").unwrap_or(input))
+            .map_err(|_| CliError::CliError(format!("Failed to decode hex string: {:?}", input)))?;
+        buf.extend_from_slice(&decoded_value);
+    }
+    Ok(())
+}
 
-// pub(crate) fn dump_data_hex<W: Write>(w: &mut W, data: &[u8]) -> Result<(), std::io::Error> {
-//     w.write(b"0x")?;
-//     w.write_all(hex::encode(data).as_bytes())?;
-//     writeln!(w)
-// }
+pub(crate) fn dump_data_hex<W: Write>(w: &mut W, data: &[u8]) -> Result<(), std::io::Error> {
+    w.write(b"0x")?;
+    w.write_all(hex::encode(data).as_bytes())?;
+    writeln!(w)
+}
