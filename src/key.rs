24ebeeaf572b343b1c123bfe6876d3fe83846e25
DimitrisPapac <dimpapach87@gmail.com>
2025-01-27 23:50:29 +0100

    Added a few unit tests for try_from

--- src/key.rs
+++ src/key.rs
@@ -620,7 +620,7 @@ impl<H: CurveHooks> TryFrom<&[u8]> for VerificationKey<H> {
             read_u32(raw_vk).map_err(|_| VerificationKeyError::InvalidNumberOfPublicInputs)?; // TODO
 
         let (_num_commitments, raw_vk) = match read_u32(raw_vk) {
-            Ok((23, raw_vk)) => (23, raw_vk),
+            Ok((23u32, raw_vk)) => (23u32, raw_vk),
             _ => Err(VerificationKeyError::InvalidCommitmentsNumber)?,
         };
 
@@ -869,6 +869,112 @@ mod should {
         )
     }
 
+    #[fixture]
+    fn valid_raw_vk() -> [u8; 1715] {
+        hex_literal::hex!(
+            "
+            00000002
+            00000010
+            00000001
+            00000017
+            00000004
+            49445f31
+            068ae63477ca649fffc34e466c212c208b89ff7dfebff7831183169ea0cfd64d
+            0d44dc459b23e94ce13c419e7feeb1d4bb61991ce667557d0ecc1ee6c29b3c3b
+            00000004
+            49445f32
+            093cf3ec6e1328ec2e9963bae3f0769bd8eb45e32cb91e2435d33daf3b336ea9
+            29432aa4a2a667ca8a6781517f689f573e78164764701f7190e07eeb282d7752
+            00000004
+            49445f33
+            211045f9f4618ac7e73d1ba72682487e558f73d6737ff3645a9824352fb90e51
+            012d9c85c11bcc8b2407f4764c4209c06e9027d21764554f5a20e9361d4d94ba
+            00000004
+            49445f34
+            2eea648c8732596b1314fe2a4d2f05363f0c994e91cecad25835338edee2294f
+            0ab49886c2b94bd0bd3f6ed1dbbe2cb2671d2ae51d31c1210433c3972bb64578
+            00000003
+            515f31
+            1a8732b002f568683304140deecc1ca5ce2553c9988950ea13c198f1afe44e13
+            2c44ea8c14491b4acc57cc74ead43131d09e58937ae057f69f29b4af8ecc3441
+            00000003
+            515f32
+            1eebbe1207643a8bd1669b999e82265d340a5ecb1a33c0b7055734ef91200c97
+            2f08a6a07ed616c588bcf4e3555c006b27d5d1ffba12754d0718481e1a9a419a
+            00000003
+            515f33
+            2a7e71e447b5645910a429e7f48f1a5deba7f7d446b95a5edd242b55f67993d3
+            2b1ea7f7453a8c80a89a675245da0c33db05ba8e95ecea432ab85f6b2d6a1e86
+            00000003
+            515f34
+            02d6fd9e84dbe74b7531e1801405a1c292117b1a17fefe9de0bfd9edf1a84bf9
+            293c6ab3c06a0669af13393a82c60a459a3b2a0b768da45ac7af7f2aec40fc42
+            0000000c
+            515f41524954484d45544943
+            18c3e78f81e83b52719158e4ac4c2f4b6c55389300451eb2a2deddf244129e7a
+            0002e9c902fe5cd49b64563cadf3bb8d7beb75f905a5894e18d27c42c62fd797
+            00000005
+            515f415558
+            155a0f51fec78c33ffceb7364d69d7ac27e570ae50bc180509764eb3fef94815
+            1c1c4720bed44a591d97cbc72b6e44b644999713a8d3c66e9054aa5726324c76
+            00000003
+            515f43
+            117d457bfb28869ab380fd6e83133eeb5b6ab48e5df1ae9bc204b60817006655
+            2a958a537a99428a1019fd2c8d6b97c48f3e74ad77f0e2c63c9dfb6dccf9a29c
+            0000000a
+            515f454c4c4950544943
+            0ad34b5e8db72a5acf4427546c7294be6ed4f4d252a79059e505f9abc1bdf3ed
+            1e5b26790a26eb340217dd9ad28dbf90a049f42a3852acd45e6f521f24b4900e
+            00000003
+            515f4d
+            0efe5ad29f99fce939416b6638dff26c845044cca9a2d9dbf94039a11d999aaa
+            0a44bf49517a4b66ae6b51eee6ac68587f768022c11ac8e37cd9dce243d01ef2
+            00000006
+            515f534f5254
+            2cbce7beee3076b78dace04943d69d0d9e28aa6d00e046852781a5f20816645c
+            2bc27ec2e1612ea284b08bcc55b6f2fd915d11bfedbdc0e59de09e5b28952080
+            00000007
+            5349474d415f31
+            210fa88bc935d90241f733cc4f011893a7d349075a0de838001178895da2aa39
+            1d270bb763cb26b2438b0760dfc7fb68fc98f87155867a2cf5c4b4ba06f637a6
+            00000007
+            5349474d415f32
+            163a9c8b67447afccc64e9ccba9d9e826ba5b1d1ddd8d6bb960f01cd1321a169
+            19256311d43dbc795f746c63b209667653a773088aba5c6b1337f435188d72c4
+            00000007
+            5349474d415f33
+            1aa81f5a2a21e5f2ce127892122ad0d3c35ac30e8556f343a85b66bb0207b055
+            2402d1ec00759182e950c3193c439370013802e6819544320a08b8682727f6c6
+            00000007
+            5349474d415f34
+            2e6367e7e914347a3bb11215add814670b848a66aa5c015faedb4f2cef37454f
+            17609c6252f021456896ab4c02adc333912c2f58020c8e55fb2e52096185a0bf
+            00000007
+            5441424c455f31
+            02c397073c8abce6d4140c9b961209dd783bff1a1cfc999bb29859cfb16c46fc
+            2b7bba2d1efffce0d033f596b4d030750599be670db593af86e1923fe8a1bb18
+            00000007
+            5441424c455f32
+            2c71c58b66498f903b3bbbda3d05ce8ffb571a4b3cf83533f3f71b99a04f6e6b
+            039dce37f94d1bbd97ccea32a224fe2afaefbcbd080c84dcea90b54f4e0a858f
+            00000007
+            5441424c455f33
+            27dc44977efe6b3746a290706f4f7275783c73cfe56847d848fd93b63bf32083
+            0a5366266dd7b71a10b356030226a2de0cbf2edc8f085b16d73652b15eced8f5
+            00000007
+            5441424c455f34
+            136097d79e1b0ae373255e8760c49900a7588ec4d6809c90bb451005a3de3077
+            13dd7515ccac4095302d204f06f0bff2595d77bdf72e4acdb0b0b43969860d98
+            0000000a
+            5441424c455f54595045
+            16ff3501369121d410b445929239ba057fe211dad1b706e49a3b55920fac20ec
+            1e190987ebd9cf480f608b82134a00eb8007673c1ed10b834a695adf0068522a
+            00
+            00
+            "
+        )
+    }
+
     #[rstest]
     fn deserialize_serialize_solidity_vk(valid_vk: [u8; VK_SIZE]) {
         let deserialized_vk =
@@ -877,8 +983,37 @@ mod should {
         pretty_assertions::assert_eq!(valid_vk, vk.as_slice())
     }
 
+    #[rstest]
+    fn deserialize_serialize_raw_vk(valid_vk: [u8; VK_SIZE], valid_raw_vk: [u8; 1715]) {
+        let deserialized_raw_vk =
+            VerificationKey::<CurveHooksImpl>::try_from(&valid_raw_vk[..]).unwrap();
+        let vk = deserialized_raw_vk.as_solidity_bytes();
+        pretty_assertions::assert_eq!(valid_vk, vk.as_slice())
+    }
+
     mod reject {
         use super::*;
+
+        #[rstest]
+        fn a_vk_from_a_short_buffer() {
+            let invalid_vk = [0u8; 10];
+
+            assert_eq!(
+                VerificationKey::<CurveHooksImpl>::try_from_solidity_bytes(&invalid_vk[..])
+                    .unwrap_err(),
+                VerificationKeyError::BufferTooShort
+            );
+        }
+
+        #[rstest]
+        fn a_vk_from_a_short_raw_buffer() {
+            let invalid_vk = [0u8; 10];
+
+            assert_eq!(
+                VerificationKey::<CurveHooksImpl>::try_from(&invalid_vk[..]).unwrap_err(),
+                VerificationKeyError::BufferTooShort
+            );
+        }
 
         #[rstest]
         fn a_vk_with_invalid_circuit_type(valid_vk: [u8; VK_SIZE]) {
@@ -889,6 +1024,18 @@ mod should {
             assert_eq!(
                 VerificationKey::<CurveHooksImpl>::try_from_solidity_bytes(&invalid_vk[..])
                     .unwrap_err(),
+                VerificationKeyError::InvalidCircuitType
+            );
+        }
+
+        #[rstest]
+        fn a_raw_vk_with_invalid_circuit_type(valid_raw_vk: [u8; 1715]) {
+            let mut invalid_vk = [0u8; 1715];
+            invalid_vk.copy_from_slice(&valid_raw_vk);
+            invalid_vk[3] = 3;
+
+            assert_eq!(
+                VerificationKey::<CurveHooksImpl>::try_from(&invalid_vk[..]).unwrap_err(),
                 VerificationKeyError::InvalidCircuitType
             );
         }
@@ -1046,17 +1193,6 @@ mod should {
                 VerificationKey::<CurveHooksImpl>::try_from_solidity_bytes(&invalid_vk[..])
                     .unwrap_err(),
                 VerificationKeyError::PointNotOnCurve { field: "ID_1" }
-            );
-        }
-
-        #[rstest]
-        fn a_vk_from_a_short_buffer() {
-            let invalid_vk = [0u8; 10];
-
-            assert_eq!(
-                VerificationKey::<CurveHooksImpl>::try_from_solidity_bytes(&invalid_vk[..])
-                    .unwrap_err(),
-                VerificationKeyError::BufferTooShort
             );
         }
     }
